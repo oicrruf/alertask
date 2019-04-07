8cffb8d8716032114469c8a3cd4d2f5ceac2ebb1
oicrruf <vm.reyesal@gmail.com>
2019-04-06 19:12:00 -0600

    Aplicando: Gull Admin-Theme

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,91 @@ Auth::routes();
 Route::get('/home', 'HomeController@home')->name('home');
 
 Route::get('/dashboard', 'HomeController@admin')->name('dashboard');
+
+//Theme Admin Gull 
+Route::get('/gull', function () {
+    return view('dashboard.dashboardv1');
+})->middleware('auth');
+// Route::view('/', 'starter')->name('starter');
+Route::get('gull/large-compact-sidebar/dashboard/dashboard1', function () {
+    // set layout sesion(key)
+    session(['layout' => 'compact']);
+    return view('dashboard.dashboardv1');
+})->name('compact')->middleware('auth');
+
+Route::get('gull/large-sidebar/dashboard/dashboard1', function () {
+    // set layout sesion(key)
+    session(['layout' => 'normal']);
+    return view('dashboard.dashboardv1');
+})->name('normal')->middleware('auth');
+
+Route::get('gull/horizontal-bar/dashboard/dashboard1', function () {
+    // set layout sesion(key)
+    session(['layout' => 'horizontal']);
+    return view('dashboard.dashboardv1');
+})->name('horizontal')->middleware('auth');
+
+
+Route::view('gull/dashboard1', 'dashboard.dashboardv1')->name('dashboard_version_1')->middleware('auth');
+Route::view('gull/dashboard2', 'dashboard.dashboardv2')->name('dashboard_version_2')->middleware('auth');
+Route::view('gull/dashboard3', 'dashboard.dashboardv3')->name('dashboard_version_3')->middleware('auth');
+Route::view('gull/dashboard4', 'dashboard.dashboardv4')->name('dashboard_version_4')->middleware('auth');
+
+// uiKits
+Route::view('gull/uikits/alerts', 'uiKits.alerts')->name('alerts')->middleware('auth');
+Route::view('gull/uikits/accordion', 'uiKits.accordion')->name('accordion')->middleware('auth');
+Route::view('gull/uikits/buttons', 'uiKits.buttons')->name('buttons')->middleware('auth');
+Route::view('gull/uikits/badges', 'uiKits.badges')->name('badges')->middleware('auth');
+Route::view('gull/uikits/carousel', 'uiKits.carousel')->name('carousel')->middleware('auth');
+Route::view('gull/uikits/lists', 'uiKits.lists')->name('lists')->middleware('auth');
+Route::view('gull/uikits/pagination', 'uiKits.pagination')->name('pagination')->middleware('auth');
+Route::view('gull/uikits/popover', 'uiKits.popover')->name('popover')->middleware('auth');
+Route::view('gull/uikits/progressbar', 'uiKits.progressbar')->name('progressbar')->middleware('auth');
+Route::view('gull/uikits/tables', 'uiKits.tables')->name('tables')->middleware('auth');
+Route::view('gull/uikits/tabs', 'uiKits.tabs')->name('tabs')->middleware('auth');
+Route::view('gull/uikits/tooltip', 'uiKits.tooltip')->name('tooltip')->middleware('auth');
+Route::view('gull/uikits/modals', 'uiKits.modals')->name('modals')->middleware('auth');
+Route::view('gull/uikits/NoUislider', 'uiKits.NoUislider')->name('NoUislider')->middleware('auth');
+Route::view('gull/uikits/cards', 'uiKits.cards')->name('cards')->middleware('auth');
+Route::view('gull/uikits/cards-metrics', 'uiKits.cards-metrics')->name('cards-metrics')->middleware('auth');
+
+// extra kits
+Route::view('gull/extrakits/imageCroper', 'extraKits.imageCroper')->name('imageCroper')->middleware('auth');
+Route::view('gull/extrakits/loader', 'extraKits.loader')->name('loader')->middleware('auth');
+Route::view('gull/extrakits/laddaButton', 'extraKits.laddaButton')->name('laddaButton')->middleware('auth');
+Route::view('gull/extrakits/toastr', 'extraKits.toastr')->name('toastr')->middleware('auth');
+Route::view('gull/extrakits/sweetAlert', 'extraKits.sweetAlert')->name('sweetAlert')->middleware('auth');
+Route::view('gull/extrakits/tour', 'extraKits.tour')->name('tour')->middleware('auth');
+Route::view('gull/extrakits/upload', 'extraKits.upload')->name('upload')->middleware('auth');
+
+
+// Apps
+Route::view('gull/apps/invoice', 'apps.invoice')->name('invoice')->middleware('auth');
+Route::view('gull/apps/inbox', 'apps.inbox')->name('inbox')->middleware('auth');
+Route::view('gull/apps/chat', 'apps.chat')->name('chat')->middleware('auth');
+Route::view('gull/apps/calendar', 'apps.calendar')->name('calendar')->middleware('auth');
+
+// forms
+Route::view('gull/forms/smartWizard', 'forms.smartWizard')->name('smartWizard')->middleware('auth');
+Route::view('gull/forms/tagInput', 'forms.tagInput')->name('tagInput')->middleware('auth');
+Route::view('gull/forms/forms-basic', 'forms.forms-basic')->name('forms-basic')->middleware('auth');
+Route::view('gull/forms/form-layouts', 'forms.form-layouts')->name('form-layouts')->middleware('auth');
+Route::view('gull/forms/form-input-group', 'forms.form-input-group')->name('form-input-group')->middleware('auth');
+Route::view('gull/forms/form-validation', 'forms.form-validation')->name('form-validation')->middleware('auth');
+Route::view('gull/forms/form-editor', 'forms.form-editor')->name('form-editor')->middleware('auth');
+
+// Charts
+Route::view('gull/charts/echarts', 'charts.echarts')->name('echarts')->middleware('auth');
+
+// datatables
+Route::view('gull/datatables/basic-tables', 'datatables.basic-tables')->name('basic-tables')->middleware('auth');
+
+// sessions
+Route::view('gull/sessions/signIn', 'sessions.signIn')->name('signIn')->middleware('auth');
+Route::view('gull/sessions/signUp', 'sessions.signUp')->name('signUp')->middleware('auth');
+Route::view('gull/sessions/forgot', 'sessions.forgot')->name('forgot')->middleware('auth');
+
+// others
+Route::view('gull/others/notFound', 'others.notFound')->name('notFound')->middleware('auth');
+Route::view('gull/others/user-profile', 'others.user-profile')->name('user-profile')->middleware('auth');
+Route::view('gull/others/starter', 'starter')->name('starter')->middleware('auth');
